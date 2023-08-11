4819f900e2623a4ae717c8e11d882b42fe8e5eae
Thanh Bui <thanh.bui1995@gmail.com>
2023-08-11 17:05:48 +0300

    model for service alert

--- hsl_server/models.py
+++ hsl_server/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class ServiceAlert(models.Model):
+    headerText = models.CharField(max_length=255)
+    descriptionText = models.CharField(max_length=255)
+    entityId = models.CharField(max_length=10)
