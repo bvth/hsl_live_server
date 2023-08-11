200fffb4fc1f5678f1d6cbc4361e1c383834276d
Thanh Bui <thanh.bui1995@gmail.com>
2023-08-11 16:13:18 +0300

    APIs for trip updates and service alerts

--- hsl_server/urls.py
+++ hsl_server/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("vehicle-positions", views.fetch_vehicle_position)
+    path("vehicle-positions", views.fetch_vehicle_position),
+    path("service-alerts", views.fetch_service_alert),
+    path("trip-updates", views.fetch_trip_updates)
 ]
--- hsl_server/views.py
+++ hsl_server/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 import requests
 
+
 # Create your views here.
 
 def index(request):
@@ -9,4 +10,14 @@ def index(request):
 
 def fetch_vehicle_position(request):
     response = requests.get("https://realtime.hsl.fi/realtime/vehicle-positions/v2/hsl")
+    return HttpResponse(response)
+
+
+def fetch_service_alert(request):
+    response = requests.get("https://realtime.hsl.fi/realtime/service-alerts/v2/hsl")
+    return HttpResponse(response)
+
+
+def fetch_trip_updates(request):
+    response = requests.get("https://realtime.hsl.fi/realtime/trip-updates/v2/hsl")
     return HttpResponse(response)
