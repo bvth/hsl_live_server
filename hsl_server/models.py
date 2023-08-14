a2f54ec38cf06c373f77bc9bebf364a2d6c0f8dc
Thanh Bui <thanh.bui1995@gmail.com>
2023-08-14 16:32:15 +0300

    POST api for /service-alerts, update ServiceAlert model

--- hsl_server/models.py
+++ hsl_server/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class ServiceAlert(models.Model):
-    headerText = models.CharField(max_length=255)
-    descriptionText = models.CharField(max_length=255)
-    entityId = models.CharField(max_length=10)
+    headerText_en = models.CharField(max_length=255)
+    descriptionText_en = models.CharField(max_length=255)
+    entityId = models.CharField(max_length=50)
+    start = models.CharField(max_length=255)
+    end = models.CharField(max_length=255)
--- hsl_server/views.py
+++ hsl_server/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
+import json
+
 import requests
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from hsl_server.models import ServiceAlert
 
 
 # Create your views here.
@@ -13,10 +18,20 @@ def fetch_vehicle_position(request):
     return HttpResponse(response)
 
 
+@csrf_exempt
 def fetch_service_alert(request):
-    response = requests.get("https://realtime.hsl.fi/realtime/service-alerts/v2/hsl")
-    return HttpResponse(response)
-
+    if request.method == "GET":
+        response = requests.get("https://realtime.hsl.fi/realtime/service-alerts/v2/hsl")
+        return HttpResponse(response)
+    if request.method == "POST":
+        json_data = json.loads(request.body)
+        alert = ServiceAlert(headerText_en=json_data["headerText_en"],
+                             descriptionText_en=json_data["descriptionText_en"],
+                             entityId=json_data["entityId"],
+                             start=json_data["start"],
+                             end=json_data["end"])
+        alert.save()
+        return HttpResponse("Done")
 
 def fetch_trip_updates(request):
     response = requests.get("https://realtime.hsl.fi/realtime/trip-updates/v2/hsl")
